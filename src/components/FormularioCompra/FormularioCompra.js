c83cbc269bbf18da5fd93d3ddfcb4c78ce9c7f33
Agustin Lorenzi <agustinlorenzi6@gmail.com>
2023-02-28 18:49:07 -0300

    funciona orden de compra

--- src/components/FormularioCompra/FormularioCompra.js
+++ src/components/FormularioCompra/FormularioCompra.js
@@ -1,40 +1,64 @@
 import React, { useState } from 'react'
 import { Button } from 'react-bootstrap'
+//import { Button } from 'react-bootstrap'
 //import { useCartContext } from '../../Context/CartContext'
 import OrdenCompra from '../OrdenCompra/OrdenCompra'
+
 
 const FormularioCompra = () => {
     // const { } = useCartContext()
     const [dataForm, setDataForm] = useState({ nombre: "", phone: "", email: "" })
-    const[aviso,setAviso] = useState("")
-    
+    const [aviso, setAviso] = useState("")
+    const [input, setInput] = useState("button")
+
 
     const tomarInput = (e) => {
         console.log("toma la funcion")
         setDataForm({ ...dataForm, [e.target.name]: e.target.value })
+        //corroborarDatos()
         console.log(e.target.name)
         console.log(e.target.value)
     }
     console.log("ver este console" + dataForm.nombre)
-    
-    function corroborarDatos() {        
-        console.log("se activa funcion corroborar")        
+
+    function corroborarDatos() {
+        console.log("se activa funcion corroborar")
         if (dataForm.nombre === "") {
             setAviso("completar nombre")
         } else if (dataForm.phone === "") {
-            setAviso ("completar telefono")
+            setAviso("completar telefono")
         } else if (dataForm.email === "") {
-            setAviso ("completar email")
+            setAviso("completar email")
         } else {
-            setAviso("datos completos") 
-
-            // vaciarCarrito()
+            setAviso("datos completos")
+            setInput("otracosa")
         }
     }
 
     return (
         <div>
-            <div>
+            {input === "button" ?
+                <div>
+                    <p>Por Favor complete sus datos de contacto.</p>
+                    <br></br>
+                    <input type="text" name="nombre" value={dataForm.nombre} onChange={tomarInput} placeholder="nombre" />
+                    <br></br>
+                    <input type="number" name="phone" value={dataForm.phone} onChange={tomarInput} placeholder="telefono" />
+                    <br></br>
+                    <input type="text" name="email" value={dataForm.email} onChange={tomarInput} placeholder="email" />
+                    <br></br>
+                    {aviso}
+                    <br></br>
+                    <Button onClick={()=>corroborarDatos()}>Confirmar Datos</Button>
+                </div> :
+                <OrdenCompra dataForm={dataForm}/>
+                
+            }
+        </div>
+        /*<div>
+            <div>            
+                <p>Por Favor complete sus datos de contacto.</p>
+                <br></br>
                 <input type="text" name="nombre" value={dataForm.nombre} onChange={tomarInput} placeholder="nombre" />
                 <br></br>
                 <input type="number" name="phone" value={dataForm.phone} onChange={tomarInput} placeholder="telefono" />
@@ -43,11 +67,13 @@ const FormularioCompra = () => {
             </div>
             <br></br>
             {aviso}
-            <Button onClick={()=>corroborarDatos()}>Confirmar Pedido</Button>
+            <br></br>
+            {input === "button" ?  <Button onClick={()=>corroborarDatos()}>Confirmar Datos</Button> : null}
             {aviso === "datos completos" ? <OrdenCompra dataForm={dataForm}/> : null}
-            {/* <button className='boton' onClick={() => { onAdd(count, cant); mostrar() }}>Agregar al carrito</button> */}
-        </div>
+            
+        </div>*/
     )
 }
 
 export default FormularioCompra
+
--- src/components/OrdenCompra/OrdenCompra.js
+++ src/components/OrdenCompra/OrdenCompra.js
@@ -1,24 +1,85 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
 import { useCartContext } from '../../Context/CartContext'
+import { getFirestore, collection, addDoc, updateDoc, doc} from "firebase/firestore"
+import { Button } from 'react-bootstrap'
+//import { Button } from 'react-bootstrap'
 
-const OrdenCompra = ({dataForm}) => {
+const OrdenCompra = ({ dataForm }) => {
 
-  const {vaciarCarrito } = useCartContext()
+  //let articulo = {}
 
-  let orden = {}
-  orden.buyer = { nombre : `${dataForm.nombre}` , phone : `${dataForm.phone}`,email:`${dataForm.email} `}
+  const { cartList, vaciarCarrito, totalCarro } = useCartContext()
 
-  console.log("objeto en orden de compra " + orden)
-  
+  function OrdenCompra() {
+    const db = getFirestore()
+
+    cartList.forEach((art) => {
+      /*const dbQuery = doc(db, "articulos", `${art.id}`)
+      getDoc(dbQuery)
+        .then((res) => {
+          articulo = res;
+
+        });*/
+
+      const queryItem = doc(db, "articulos", `${art.id}`)
+      updateDoc(queryItem, { stock: 2})
+
+
+    })
+
+
+
+    let orden = {}
+
+    //datos comprador
+    orden.buyer = { nombre: `${dataForm.nombre}`, phone: `${dataForm.phone}`, email: `${dataForm.email} ` }
+    console.log("objeto en orden de compra " + orden.buyer.nombre)
+
+    //productos del carro
+    orden.items = cartList.map(cartItem => {
+      const id = cartItem.id
+      const nombre = cartItem.nombre
+      const cantidad = cartItem.cantidad
+      const precio = cartItem.precio
+      const subtotal = cartItem.subtotal
+
+      return { id, nombre, cantidad, precio, subtotal }
+    })
+
+    //Composicion de la Fecha
+    const DIAS = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+    const MESES = [
+      "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+      "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre",
+    ];
+    let diaSemana = DIAS[(new Date()).getDay()];
+    let diaMes = ((new Date()).getDate())
+    let mes = MESES[(new Date()).getMonth()];
+    let año = (new Date()).getFullYear();
+    orden.date = ("Hoy es " + diaSemana + " " + diaMes + " de " + mes + " del " + año + ".")
+
+    //Total del Carro.
+    orden.total = totalCarro()
+
+    //grabar orden de compra en firebase
+
+    const queryCollection = collection(db, "orders")
+    addDoc(queryCollection, orden)
+      .then(resp => console.log(resp))
+      .catch(err => console.log(err))
+      .finally(() => vaciarCarrito())
+  }
+
+
   return (
     <div>
-      <h1>OrdenCompra</h1>
-      <Button onClick={() => { vaciarCarrito() }}>Vaciar Carrito</Button>
+      <Button onClick={() => OrdenCompra()}>Confirmar Pedido</Button>
+      {/* {OrdenCompra()} */}
 
 
     </div>
   )
+
 }
+
 
 export default OrdenCompra
